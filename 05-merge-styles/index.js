8fb990091d2fa515fc908ad349fc41b3fcb11253
srgMet <100075467+srgMet@users.noreply.github.com>
2022-05-24 22:04:40 +0300

    feat: 5task

---
+++ 05-merge-styles/index.js
@@ -0,0 +1,20 @@
+const fs = require('fs');
+const path = require('path');
+
+const bundle = fs.createWriteStream(
+   path.resolve(__dirname, 'project-dist', 'bundle.css')
+);
+
+fs.readdir(path.resolve(__dirname, 'styles'), (err, files) => {
+   if (!err) {
+      files.forEach((file) => {
+         let style = '';
+         if (path.extname(file) == '.css') {
+            const stream = fs.createReadStream(
+               path.resolve(__dirname, 'styles', file)
+            );
+            stream.on('data', (chunk) => bundle.write((style += chunk)));
+         }
+      });
+   }
+});
